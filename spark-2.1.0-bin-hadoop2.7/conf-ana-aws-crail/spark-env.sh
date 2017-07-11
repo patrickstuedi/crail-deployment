74f1a6ea11e88edcb5c8e5cc7c92a3bfced9612f
Ubuntu <ubuntu@ip-10-0-142-89.us-west-2.compute.internal>
2017-07-11 23:24:52 +0000

    spark config

--- spark-2.1.0-bin-hadoop2.7/conf-ana-aws-crail/spark-env.sh
+++ spark-2.1.0-bin-hadoop2.7/conf-ana-aws-crail/spark-env.sh
@@ -42,7 +42,7 @@
 # - SPARK_DAEMON_JAVA_OPTS, to set config properties for all daemons (e.g. "-Dx=y")
 # - SPARK_PUBLIC_DNS, to set the public dns name of the master or workers
 
-HADOOP_CONF_DIR=/dataL/crail/crail-deployment/hadoop/etc/hadoop
+HADOOP_CONF_DIR=/home/ubuntu/crail-deployment/hadoop/etc/hadoop
 #SPARK_LOCAL_IP=$(hostname | cut -d"." -f1 | cut -d"-" -f1)-ib
 #SPARK_PUBLIC_DNS=zac01-ib
 
