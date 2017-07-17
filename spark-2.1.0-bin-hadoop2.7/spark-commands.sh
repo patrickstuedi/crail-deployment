f9bff8c22ede2501fc7c5bb7bc5936870a90391c
Ana Klimovic <ana.klimov@gmail.com>
2017-07-17 06:42:16 +0000

    add spark commands script for terasort vanilla

--- spark-2.1.0-bin-hadoop2.7/spark-commands.sh
+++ spark-2.1.0-bin-hadoop2.7/spark-commands.sh
@@ -13,3 +13,7 @@
 
 
 ./bin/spark-submit --master yarn --deploy-mode client --num-executors 8 --executor-cores 8 --executor-memory 48G --driver-memory 48G --class com.github.ehiggs.spark.terasort.TeraGen /home/ubuntu/crail-deployment/spark/apps/jars-ana/spark-terasort-1.0-jar-with-dependencies.jar 6000g data/terasort_in_6000g
+
+./bin/spark-submit --master yarn --deploy-mode client --num-executors 8 --executor-cores 8 --executor-memory 48G --driver-memory 48G --class com.github.ehiggs.spark.terasort.TeraSort /home/ubuntu/crail-deployment/spark/apps/jars-ana/spark-terasort-1.0-jar-with-dependencies.jar data/terasort_in_6000g data/terasort_out_6000g
+
+./bin/spark-submit --master yarn --deploy-mode client --num-executors 1 --executor-cores 8 --executor-memory 48G --driver-memory 48G --class com.github.ehiggs.spark.terasort.TeraSort /home/ubuntu/crail-deployment/spark/apps/jars-ana/spark-terasort-1.0-jar-with-dependencies.jar data/terasort_in_750g data/terasort_out_750g
