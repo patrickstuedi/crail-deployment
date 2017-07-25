f351406610944d8b699dc68d137c54466e3dda2e
Ana Klimovic <ana.klimov@gmail.com>
2017-07-25 22:55:41 +0000

    udpate hdfs config for disk spread on d2 instances

--- ana-aws-setup.sh
+++ ana-aws-setup.sh
@@ -54,8 +54,106 @@ mkdir /mnt/sdc/hdfsdir/dfs
 mkdir /mnt/sdc/hdfsdir/dfs/data
 mkdir /mnt/sdc/hadoop-tmp
 
+sudo mkfs.ext4 /dev/xvdd
+sudo mount /dev/xvdb /mnt/sdd 
+sudo chmod a+rw /mnt/sdd
+mkdir /mnt/sdd/yarn
+mkdir /mnt/sdd/yarn/local-dir
+mkdir /mnt/sdd/hdfsdir
+mkdir /mnt/sdd/hdfsdir/dfs
+mkdir /mnt/sdd/hdfsdir/dfs/data
+mkdir /mnt/sdd/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvde
+sudo mount /dev/xvdc /mnt/sde
+sudo chmod a+rw /mnt/sde
+mkdir /mnt/sde/yarn
+mkdir /mnt/sde/yarn/local-dir
+mkdir /mnt/sde/hdfsdir
+mkdir /mnt/sde/hdfsdir/dfs
+mkdir /mnt/sde/hdfsdir/dfs/data
+mkdir /mnt/sde/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdf
+sudo mount /dev/xvdb /mnt/sdf
+sudo chmod a+rw /mnt/sdf
+mkdir /mnt/sdf/yarn
+mkdir /mnt/sdf/yarn/local-dir
+mkdir /mnt/sdf/hdfsdir
+mkdir /mnt/sdf/hdfsdir/dfs
+mkdir /mnt/sdf/hdfsdir/dfs/data
+mkdir /mnt/sdf/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdg
+sudo mount /dev/xvdc /mnt/sdg
+sudo chmod a+rw /mnt/sdg
+mkdir /mnt/sdg/yarn
+mkdir /mnt/sdg/yarn/local-dir
+mkdir /mnt/sdg/hdfsdir
+mkdir /mnt/sdg/hdfsdir/dfs
+mkdir /mnt/sdg/hdfsdir/dfs/data
+mkdir /mnt/sdg/hadoop-tmp
 
 
+sudo mkfs.ext4 /dev/xvdh
+sudo mount /dev/xvdb /mnt/sdh 
+sudo chmod a+rw /mnt/sdh
+mkdir /mnt/sdh/yarn
+mkdir /mnt/sdh/yarn/local-dir
+mkdir /mnt/sdh/hdfsdir
+mkdir /mnt/sdh/hdfsdir/dfs
+mkdir /mnt/sdh/hdfsdir/dfs/data
+mkdir /mnt/sdh/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdi
+sudo mount /dev/xvdc /mnt/sdi
+sudo chmod a+rw /mnt/sdi
+mkdir /mnt/sdi/yarn
+mkdir /mnt/sdi/yarn/local-dir
+mkdir /mnt/sdi/hdfsdir
+mkdir /mnt/sdi/hdfsdir/dfs
+mkdir /mnt/sdi/hdfsdir/dfs/data
+mkdir /mnt/sdi/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdj
+sudo mount /dev/xvdb /mnt/sdj 
+sudo chmod a+rw /mnt/sdj
+mkdir /mnt/sdj/yarn
+mkdir /mnt/sdj/yarn/local-dir
+mkdir /mnt/sdj/hdfsdir
+mkdir /mnt/sdj/hdfsdir/dfs
+mkdir /mnt/sdj/hdfsdir/dfs/data
+mkdir /mnt/sdj/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdk
+sudo mount /dev/xvdc /mnt/sdk
+sudo chmod a+rw /mnt/sdk
+mkdir /mnt/sdk/yarn
+mkdir /mnt/sdk/yarn/local-dir
+mkdir /mnt/sdk/hdfsdir
+mkdir /mnt/sdk/hdfsdir/dfs
+mkdir /mnt/sdk/hdfsdir/dfs/data
+mkdir /mnt/sdk/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdl
+sudo mount /dev/xvdb /mnt/sdl
+sudo chmod a+rw /mnt/sdl
+mkdir /mnt/sdl/yarn
+mkdir /mnt/sdl/yarn/local-dir
+mkdir /mnt/sdl/hdfsdir
+mkdir /mnt/sdl/hdfsdir/dfs
+mkdir /mnt/sdl/hdfsdir/dfs/data
+mkdir /mnt/sdl/hadoop-tmp
+
+sudo mkfs.ext4 /dev/xvdm
+sudo mount /dev/xvdc /mnt/sdm
+sudo chmod a+rw /mnt/sdm
+mkdir /mnt/sdm/yarn
+mkdir /mnt/sdm/yarn/local-dir
+mkdir /mnt/sdm/hdfsdir
+mkdir /mnt/sdm/hdfsdir/dfs
+mkdir /mnt/sdm/hdfsdir/dfs/data
+mkdir /mnt/sdm/hadoop-tmp
 
 ####### Launch Hadoop ##########
 ## https://hadoop.apache.org/docs/stable/hadoop-project-dist/hadoop-common/ClusterSetup.html
