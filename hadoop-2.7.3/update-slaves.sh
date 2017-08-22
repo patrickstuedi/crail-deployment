43c0c5c67620331751dc30112d5b558c53c5711c
Ana Klimovic <ana.klimov@gmail.com>
2017-08-22 00:00:27 +0000

    test terasort with 4 nodes

--- hadoop-2.7.3/update-slaves.sh
+++ hadoop-2.7.3/update-slaves.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#while read slave; do
-#  	echo "ssh into $slave"
-#	ssh $slave 'cd crail-deployment; git pull'
-#done < etc/hadoop/slaves
-
-
 cat  etc/hadoop/slaves | while read slave
 do {
   	echo "ssh into $slave"
