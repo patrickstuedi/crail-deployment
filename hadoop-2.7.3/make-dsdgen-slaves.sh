24dcd42ed0d5acb64d69090227847b57d0b56f69
Ana Klimovic <ana.klimov@gmail.com>
2017-08-27 22:39:07 +0000

    update script to install deps for dsdgen build

--- hadoop-2.7.3/make-dsdgen-slaves.sh
+++ hadoop-2.7.3/make-dsdgen-slaves.sh
@@ -3,5 +3,6 @@
 cat  etc/hadoop/slaves | while read slave
 do {
   	echo "ssh into $slave"
+	ssh -t $slave 'sudo sh -c "apt-get install gcc make flex bison byacc git"'	
 	ssh $slave 'cd crail-deployment/spark-2.1.0-download/apps/dsdgen-dir; ./make-dsdgen.sh'
 } < /dev/null; done
