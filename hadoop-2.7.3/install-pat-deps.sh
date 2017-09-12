da476c1810a540aa8858fc72e5a3fa84b0cb688f
Ana Klimovic <ana.klimov@gmail.com>
2017-09-12 07:33:44 +0000

    add script to install pat deps on all nodes

---
+++ hadoop-2.7.3/install-pat-deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat  /home/ubuntu/crail-deployment/hadoop/etc/hadoop/slaves | while read slave
+do {
+  	echo "ssh into $slave"
+	#ssh -t $slave 'sudo sh -c "apt-get install -y sysstat"'
+	ssh $slave "jps"
+} < /dev/null; done
