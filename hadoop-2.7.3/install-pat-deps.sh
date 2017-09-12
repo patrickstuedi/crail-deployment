28aba736323962f6d7b30257b455bccba2c1010e
Ubuntu <ubuntu@ip-10-0-127-190.us-west-2.compute.internal>
2017-09-12 07:34:30 +0000

    fix deps install script

--- hadoop-2.7.3/install-pat-deps.sh
+++ hadoop-2.7.3/install-pat-deps.sh
@@ -3,6 +3,6 @@
 cat  /home/ubuntu/crail-deployment/hadoop/etc/hadoop/slaves | while read slave
 do {
   	echo "ssh into $slave"
-	#ssh -t $slave 'sudo sh -c "apt-get install -y sysstat"'
-	ssh $slave "jps"
+	ssh -t $slave 'sudo sh -c "apt-get install -y sysstat"'
+	#ssh $slave "jps"
 } < /dev/null; done
